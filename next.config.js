16a8fdb6be883c357669bef5f0c3d764595f46e0
gtg7784 <tae.gun7784@gmail.com>
2021-03-09 11:05:34 +0900

    chore: add next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPlugins = require('next-compose-plugins');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withPlugins([
+  [withSass, {
+    cssModules: true,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[local]___[hash:base64:5]'
+      },
+    },
+  ]
+]);
